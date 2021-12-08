145b7973ec46cb5604ea723390df9318e6900591
imjoseangel <josea.munoz@gmail.com>
2021-12-08 23:52:24 +0100

    add signatire

--- tests/test_dnsmock.py
+++ tests/test_dnsmock.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=R1710,W0621,W0613,C0301
 
+from inspect import signature
+import socket
 import pytest
 import dnsmock
 
@@ -21,6 +23,12 @@ def mock_bind():
     return str(dnsmock.lib.etc_hosts)
 
 
+@pytest.fixture
+def mock_custom_resolver():
+    '''Test Custom Resolver'''
+    return str(signature(dnsmock.lib.custom_resolver(socket.getaddrinfo)))
+
+
 def test_version(mock_version):
     '''Test version'''
     assert mock_version == EXPECTED_VERSION
@@ -29,3 +37,8 @@ def test_version(mock_version):
 def test_bind(mock_bind):
     '''Test version'''
     assert mock_bind == "{('www.example.com', 443): [(<AddressFamily.AF_INET: 2>, <SocketKind.SOCK_STREAM: 1>, 6, '', ('127.0.0.1', 443))]}"
+
+
+def test_custom_resolver(mock_custom_resolver):
+    '''Test custom resolver'''
+    assert mock_custom_resolver == "(host, port, family=0, type=0, proto=0, flags=0)"
