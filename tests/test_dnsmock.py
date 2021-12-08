774022a9009abecc266708002f3521b19aceb03d
imjoseangel <josea.munoz@gmail.com>
2021-12-08 22:58:16 +0100

    add new test

--- tests/test_dnsmock.py
+++ tests/test_dnsmock.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=R1710,W0621,W0613,C0301
 
+from socket import AddressFamily, SocketKind
 import pytest
 import dnsmock
 
@@ -14,6 +15,18 @@ def mock_version():
     return dnsmock.VERSION
 
 
+@pytest.fixture
+def mock_bind():
+    '''Returns Socket Bind'''
+    dnsmock.bind_ip('www.example.com', 443, '127.0.0.1')
+    return str(dnsmock.lib.etc_hosts)
+
+
 def test_version(mock_version):
     '''Test version'''
     assert mock_version == EXPECTED_VERSION
+
+
+def test_bind(mock_bind):
+    '''Test version'''
+    assert mock_bind == "{('www.example.com', 443): [(<AddressFamily.AF_INET: 2>, <SocketKind.SOCK_STREAM: 1>, 6, '', ('127.0.0.1', 443))]}"
